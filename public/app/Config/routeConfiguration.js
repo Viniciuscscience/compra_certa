08afe203e8c90f3dc895b6408859e164c1b3ba71
root <root@debian>
2014-11-04 22:36:21 -0300

    branch master ok

--- public/app/Config/routeConfiguration.js
+++ public/app/Config/routeConfiguration.js
@@ -1,6 +1,11 @@
 app.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider
+            .when('/error',{
+                title: 'ERRO',
+                templateUrl: 'app/Views/error.html',
+                controller: 'homeCtrl'
+            })
                 .when('/home', {
                     title: 'HOME',
                     templateUrl: 'app/Views/home.html',
--- public/app/Controllers/homeController.js
+++ public/app/Controllers/homeController.js
@@ -2,5 +2,5 @@
 app.controller('homeCtrl', function($scope) {
 
     $scope.mensagem_generica = "ESTA MENSAGEM EH GENERICA";
-
+    $scope.mensagem_erro = "ESTA PAGINA NAO EXISTE!";
 });
