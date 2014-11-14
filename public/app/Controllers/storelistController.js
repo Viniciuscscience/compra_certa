45768e8b38feb5b66dea8ea2881a2a5c3bc50d10
Vinicius <vinicius.cscience@gmail.com>
2014-11-14 11:32:28 -0300

    Update storelistController.js
    Identação acertada

--- public/app/Controllers/storelistController.js
+++ public/app/Controllers/storelistController.js
@@ -1,7 +1,7 @@
 app.controller('storelistCtrl', function ($scope, $rootScope, $window) {
 
-
     $scope.choosedFilter = '';
+    
     $scope.filters = [
         {name: "Menor Preco", value: "price"},
         {name: "Maior Preco", value: "-price"},
@@ -9,27 +9,28 @@ app.controller('storelistCtrl', function ($scope, $rootScope, $window) {
         {name: "Maior Avaliacao", value: "-star"},
         {name: "Menor Avaliacao", value: "star"}
     ];
+    
     $scope.deleteStore = function (name) {
         $rootScope.storelist.forEach(function (_store) {
             if (_store.store == name )
                 _store.price = -1;
         });
     };
+    
     $scope.deleteProduct = function (name, price, descr) {
     	 var index = 0;
     	 for ( index = 0; index < $rootScope.storelist.length; index++) {
          	if ( $rootScope.storelist[index].store == name && 
          	     $rootScope.storelist[index].price == price && 
          	     $rootScope.storelist[index].description == descr ) {
-         			
          			break;
          	}
          }       
-         console.log("index is " + index);
          if (index < $rootScope.storelist.length) {
     		$rootScope.storelist.splice(index,1);
     	 }
     };
+    
     $scope.buyProduct = function (website) {
         $window.open(website);
     };
