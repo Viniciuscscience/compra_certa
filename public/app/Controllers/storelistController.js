07f9ec7b1a3689f52db83ef233521313c32211d2
MateusGondndim <mateusgondimlima@gmail.com>
2014-11-18 22:05:22 -0200

    subindo mudanças para bug_sort

--- public/app/Controllers/storelistController.js
+++ public/app/Controllers/storelistController.js
@@ -8,7 +8,7 @@ app.controller('storelistCtrl', function ($scope, $rootScope, $window) {
     $scope.choosedFilter = '';
     
     $scope.filters = [
-        {name: "Menor Preco", value: ""},
+        {name: "Menor Preco", value: "price"},
         {name: "Maior Preco", value: "-price"},
         {name: "Alfabetica", value: "store"},
         {name: "Maior Avaliacao", value: "-star"},
@@ -35,6 +35,54 @@ app.controller('storelistCtrl', function ($scope, $rootScope, $window) {
     		$rootScope.storelist.splice(index,1);
     	 }
     };
+    
+    $scope.sort = function (list,isAscending) {
+    	
+    	var  lowerThan    =  [];
+    	var  greaterThan  =  [];
+    	var  sortedArray  =  [];
+    	var  pivot        =  list[0];
+    	var  j            =  0;
+    	var  i            =  0;
+    	
+    	if(list.length == 0 )
+    	   return [];
+    	for( i = 1; i < list.length; i++) {
+    		if( list[i].price <= pivot.price) {
+    			lowerThan[j++] = list[i];
+    		}
+    	}
+    	j = 0;
+    	for( i = 1; i < list.length; i++) {
+    		if( list[i].price > pivot.price) {
+    			greaterThan[j++] = list[i];
+    		}
+    	}
+    	if(isAscending) {
+    		return sortedArray.concat($scope.sort(lowerThan,isAscending ), pivot, $scope.sort(greaterThan,isAscending) );
+    	}	    
+        else {
+           return sortedArray.concat($scope.sort(greaterThan,isAscending) ,pivot, $scope.sort(lowerThan,isAscending) );	
+        }   
+    };
+    
+    $scope.$watch('choosedFilter', function() {
+    	var i;
+    	if(!(typeof $rootScope.storelist[0] === 'undefined') ){
+    		console.log("here    " + 'choosedFilter');
+    		if('choosedFilter' == "price") {
+    			$rootScope.storelist = $scope.sort($rootScope.storelist, true);
+    	    	console.log("Ordenando em ordem cresente");    
+    		}
+        	else {
+        		$rootScope.storelist = $scope.sort($rootScope.storelist, false);
+            	console.log("Ordenando em ordem decresente");  
+        	}
+    	    for( i = 0; i < $rootScope.storelist.length; i++) {
+    		    	console.log("price : " +  $rootScope.storelist[i].price + "\n");
+    		    }
+    	}
+    });
     
     $scope.buyProduct = function (website) {
         $window.open(website);
