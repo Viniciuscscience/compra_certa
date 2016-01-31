0e984253ffc65de891740b988d6dcf727df655f0
vinicius <vinicius.cscience@gmail.com>
2016-01-31 00:33:53 -0200

    ok

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
+
 var app = angular.module('myApp', [
     'ngRoute',
     'ngCookies'
 ]);
 
 app.run(['$location', '$rootScope', function ($location, $rootScope) {
-=======
-var app = angular.module('myApp', [
-    'ngRoute',
-    'ngCookies'
-]);
 
-app.run(['$location', '$rootScope', function ($location, $rootScope) {
->>>>>>> 58baaaa6fe59c5b19d87338ce32b1771847f2663
 }]);
